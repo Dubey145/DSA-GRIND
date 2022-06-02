9f7aa87d5995cd8f81e8811db1396611e0c52520
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-06-03 04:35:07 +0530

    Add files via upload

--- 3. Pointers/pointers_and_functions.cpp
+++ 3. Pointers/pointers_and_functions.cpp
@@ -20,6 +20,24 @@ void increment_copy(int i)
         // call by value
     }
 
+void increment_pointer(int *p)
+    {
+        *p = (*p)+1;
+        //changes will reflect
+    }
+
+void sum(int arr[] , int size) 
+    {
+        //here arr acts as a pointer and sizeof(arr) = 8 not 20bytes
+        arr[0] = 2;
+        int count = 0;
+        for(int i=0; i<size; i++)
+            {
+                count += arr[i];
+            }
+        cout<<count;
+    }
+
 int main()
     {
         int i =10;
@@ -28,5 +46,11 @@ int main()
         print(&i);
         increment_copy(i); 
         print(&i);
+        increment_pointer(&i); 
+        print(&i);
+
+        int array[] = {1,1,1,1,1};
+        sum(array,5); //the base address of the array is passed
+        cout<<endl<<array[0];
         return 0;
     }
