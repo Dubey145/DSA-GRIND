44a156def6e639079b4840d50f7e5dd36fb15dab
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-06-04 09:05:08 +0530

    Add files via upload

---
+++ 4. Dynamic Memory Allocation/dma8_const.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+/*
+Creating a constant integer
+a variable can be declared constant with "const" keyword
+and no changes can be made  
+
+with declaration we have to assign a value otherwise the constant 
+variable will always keep garbage in it 
+*/
+
+int main()
+    {
+        const int i = 10;
+        //no further edits in i 
+        return 0;
+    }
