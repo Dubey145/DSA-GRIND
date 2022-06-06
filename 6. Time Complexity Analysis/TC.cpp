a35105f39b905ca563ea551595952175c3b46b2b
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-06-06 06:30:00 +0530

    Add files via upload

---
+++ 6. Time Complexity Analysis/TC.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+/*
+
+when we find the solution to a problem we want to know how good or bad the solution is in terms of time and space requirements
+
+this is why we study multiple solutions to a problem
+
+Time Complexity 
+
+if we compare 2 sorting algorithms, merge sort and selection sort for a large number of input say 1 million numbers 
+
+where all the numbers are reversed (descending order) then by experiment we can see that selection sort gets stuck and merge sort gets the sorted array in 0.5 sec. this shows how much faster merge sort is
+
+we will not perform experimental analysis for all algorithms because 
+1. if we run the program again we can get different results  
+2. generating an unbiased test case
+3. we will have to code each solution 
+4. time consuming to get the output 
+
+Big O Notation 
+we dont fine the exact time required to execute 
+instead we find the number of unit operations required 
+
+Big O notation is used to describe the complexity of an algorithm when measuring its efficiency, which in this case means how well the algorithm scales with the size of the dataset.
+*/
+
+int main() 
+    {
+
+        return 0;
+    }
