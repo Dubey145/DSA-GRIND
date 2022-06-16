a787e1c45686b066d2f437069c86621707dd6341
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-06-16 06:09:06 +0530

    Add files via upload

--- 9.Linked List/ll1.cpp
+++ 9.Linked List/ll1.cpp
@@ -31,14 +31,64 @@ class node
                             next = NULL;
                         }
                 }
+        void print(node *head) // will lose data after printing operate on a copy of head
+            {
+                //head here is a local variable
+                //changes made to iw will  not affect the head in main 
+                //but if we use head here for operations we might lose data so make a temp
+                node * temp = head;
+                while(temp != NULL) //use -> for pointers it means (*ptr).attr
+                    {
+                        cout<<temp->data<<endl;
+                        temp = temp->next;
+                    }
+            }
     };
+    
+     
 int main()
     {
-        node n1(1);
-        node n2(2);
-        node *head = &n1;
-        n1.next = &n2; // connected n1 with n2
-        cout<<head->data<<endl;
-        cout<<head->next->data;
+        // node n1(1);
+        // node n2(2);
+        // node *head = &n1;
+        // n1.next = &n2; // connected n1 with n2
+        // cout<<head->data<<endl;
+        // cout<<head->next->data;
+        // //dynamic 
+        // node *n3 = new node(3);
+        // n2.next = n3;  
+        // n3->next = NULL;
+
+        //node n1(100),n2(200),n3(300),n4(400),n5(500);
+        //node * head = &n1;
+        
+        //connecting manually
+        /*
+        n1.next = &n2;
+        n2.next = &n3;
+        n3.next = &n4; 
+        n4.next = &n5;
+        n5.next = NULL;
+        //LETS PRINT 
+        head->print(head);
+        n1.print(head); 
+        */
+        
+
+        //printing dynamically generated nodes 
+        node * head;
+        node * n1 = new node(1);
+        node * n2 = new node(2);
+        node * n3 = new node(3);
+        node * n4 = new node(4);
+        node * n5 = new node(5);
+        n1->next = n2;
+        n2->next = n3;
+        n3->next = n4; 
+        n4->next = n5;
+        n5->next = NULL;
+        head = n1;
+        head->print(head);
+
         return 0;
     }
