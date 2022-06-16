b8f0ca812760429711c1a64b93a7b3f299bd8940
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-06-17 04:58:35 +0530

    Add files via upload

---
+++ 10. Trees/tree1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+// trees are a non linear data structure 
+// file system is an example of tree data structure
+
+//like the head node in LL  we will have the root node here 
+
+class node
+    {
+        //implementing a binary tree
+        int data;
+        node *left;
+        node *right;
+    };
+
+int main()
+    {
+
+        return 0;
+    }
