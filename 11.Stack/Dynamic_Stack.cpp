0ca20b184ac61335e1c8cd06bc22f6654cc249d1
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-07-04 04:54:58 +0530

    Add files via upload

---
+++ 11.Stack/Dynamic_Stack.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+
+//dynamic stack, no constraint in size
+
+class stackDynamic 
+    {
+        int size;
+        int *data;
+        int next_index;
+
+        public:
+
+        stackDynamic()
+            {
+                size = 4;
+                data = new int[size];
+                next_index = 0;
+            }
+        //user functions
+        void display()
+            {
+                for(int i = 0; i < next_index; i++)
+                    {
+                        cout<<data[i]<<" ";
+                    }
+            }
+        int get_size()
+            {
+                return next_index; // just return the next index
+            }
+        bool is_empty()
+            {
+                return next_index == 0;
+            }
+        void push(int element)
+            {
+                if(next_index == size)
+                    {
+                        cout<<"\nStack is full, increasing size";
+                        size *=2;
+                        int * new_data = new int[size];
+                        for(int i = 0;i< size; i++)
+                            {
+                               new_data[i] = data[i];
+                            } 
+                        delete [] data;
+                        data = new_data;
+                    }
+                    data[next_index++] = element;
+            }
+        void pop()
+            {
+                if(next_index == 0)
+                    {
+                        cout<<"Stack is empty";
+                        return;
+                    }
+                else 
+                    next_index--;
+            }
+    };
+int main ()
+    {
+        stackDynamic s;
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        s.push(5);
+        s.push(6);
+        s.pop();
+        s.display();
+        
+        return 0;
+    }
