8c8f04dfcea658dd11a4b767519a0d3916861a94
Aayush Dubey <91815836+Dubey145@users.noreply.github.com>
2022-08-20 17:06:06 +0530

    Add files via upload

---
+++ maths/prime.cpp
@@ -0,0 +1,44 @@
+// problem link : prime numbers
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool prime_noob(int num)
+    {
+        if(num == 1) return false;
+
+        for(int i = 2; i <= num; i ++)
+            {
+                if(num%i == 0)
+                    {
+                        return false;
+                    }
+            }
+        return true;
+    }
+
+bool prime_better(int nums)
+    {
+        if(nums == 1) return false;
+        if(nums == 2 || nums == 3) return true;
+        if(nums%2 == 0) return false;
+
+        for(int i = 3 ;  i*i <=nums; i++)
+            {
+                if(nums%i == 0 || nums%(i+2) == 0)
+                    {
+                        return false;
+                    }
+            }
+        return true;
+    }
+//optimized time complexity O(sqrt(n))
+
+int main()
+    {
+        vector<int> nums{};
+
+
+        return 0;
+    }
